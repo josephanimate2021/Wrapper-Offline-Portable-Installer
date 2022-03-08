c1a8817fe9d849de5fa18fff614c3ab1378d1215
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-08 17:32:04 -0500

    Initial commit

---
+++ install_wrapper_offline_remastered.bat
@@ -0,0 +1,9 @@
+:: Wrapper Offline Remastered Installer
+:: Creator: Joseph Animate 2021
+@echo off
+echo Installing Wrapper Offline Remastered...
+pushd ..\..\
+call Wrapper-Offline-Remastered-Installer-main\Wrapper-Offline-Remastered-Installer-main\PortableGit\bin\git.exe clone https://github.com/ykranimate2021-ga/WrapperOfflineRemasteredV2.git
+echo Wrapper Offline Remastered Has Been Installed
+pause
+exit
