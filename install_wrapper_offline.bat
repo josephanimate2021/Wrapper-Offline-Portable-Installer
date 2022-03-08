e51839cf63e86c9d3ed1429dcc8aaa6980ce7ae8
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-08 17:58:45 -0500

    Fixed bugs and other stuff

--- install_wrapper_offline.bat
+++ install_wrapper_offline.bat
@@ -1,34 +1,70 @@
 :: Wrapper: Offline Installer
-:: Creator: Joseph Animate 2021
-@echo off
-echo Enter 1 to install the stable edition of Wrapper: Offline
-echo Enter 2 to install the beta version of Wrapper: Offline
-echo:
-set /p CHOICE=Choice:
-if "!choice!"=="0" ( exit )
-if "!choice!"=="1" goto download
-if "!choice!"=="2" goto download_beta
-echo Time to choose. && goto responces
-:responces
+:: Author: joseph the animator#2292
+:: License: MIT
+title Wrapper: Offline Installer [Initializing...]
+
+::::::::::::::::::::
+:: Initialization ::
+::::::::::::::::::::
+
+:: Stop commands from spamming stuff, cleans up the screen
+@echo off && cls
+
+:: Lets variables work or something idk im not a nerd
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Make sure we're starting in the correct folder, and that it worked (otherwise things would go horribly wrong)
+pushd "%~dp0"
+
+:: Check *again* because it seems like sometimes it doesn't go into dp0 the first time???
+pushd "%~dp0"
+
+:::::::::::::::::::::::
+:: Wrapper Installer ::
+:::::::::::::::::::::::
+
+title Wrapper: Offline Installer
+:cls
 cls
-echo Enter 1 to install the stable edition of Wrapper: Offline
-echo Enter 2 to install the beta version of Wrapper: Offline
+
+echo Wrapper: Offline Installer
+echo Project led by GoTest334. Installer is created by the Wrapper Offline Team. Installer is recreated by Joseph Animate 2022.
 echo:
+echo Enter 1 to install Wrapper: Offline
+echo Enter 2 to install Wrapper: Offline Beta
+echo Enter 0 to close the installer
+:wrapperidle
+echo:
+
+:::::::::::::
+:: Choices ::
+:::::::::::::
+
 set /p CHOICE=Choice:
 if "!choice!"=="0" goto exit
 if "!choice!"=="1" goto download
-echo Time to choose. && goto responces
+if "!choice!"=="2" goto download_beta
+echo Time to choose. && goto wrapperidle
+
 :download
 cls
-echo Installing Wrapper: Offline...
-pushd ..\..\
+pushd "..\..\"
+echo Cloning repository from GitHub...
 call Wrapper-Offline-Portable-Installer-main\Wrapper-Offline-Portable-Installer-main\PortableGit\bin\git.exe clone https://github.com/Wrapper-Offline/Wrapper-Offline.git
 echo Wrapper: Offline Has Been Installed
-goto responces
+cls
+pause
+goto cls
+
 :download_beta
 cls
-echo Installing Wrapper: Offline Beta...
-pushd ..\..\
+pushd "..\..\"
+echo Cloning repository from GitHub...
 call Wrapper-Offline-Portable-Installer-main\Wrapper-Offline-Portable-Installer-main\PortableGit\bin\git.exe clone https://github.com/Wrapper-Offline/Wrapper-Offline-Beta.git
 echo Wrapper: Offline Beta Has Been Installed
-goto responces
+cls
+pause
+goto cls
+
+:exit
+pause & exit
